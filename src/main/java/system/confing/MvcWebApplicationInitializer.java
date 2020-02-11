4d76992979a1a99a1a77943e8955effa9371eefd
Sergey Shimko <sergey.shimko@thomsonreuters.com>
2020-02-11 14:02:40 +0300

    changed to thymeleaf login form

---
+++ src/main/java/system/confing/MvcWebApplicationInitializer.java
@@ -0,0 +1,25 @@
+package system.confing;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer
+{
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses()
+    {
+        return new Class[] {AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[] {"/"};
+    }
+}
