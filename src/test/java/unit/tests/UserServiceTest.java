bc1f70d9663a1f73eef823ae313bf893bca15fc8
Sergey Shimko <sergey.shimko@thomsonreuters.com>
2020-03-12 18:50:11 +0300

    added mock test

--- src/test/java/unit/tests/UserServiceTest.java
+++ src/test/java/unit/tests/UserServiceTest.java
@@ -1,16 +1,17 @@
 package unit.tests;
 
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import system.dao.UserDao;
 import system.model.User;
-
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-public class UserServiceTest {
+public class UserServiceTest extends Mockito
+{
 
     @Test
     public void createUserTest() {
@@ -22,5 +23,12 @@ public class UserServiceTest {
         System.out.println(lastUser.toString());
         log.info(lastUser.toString());
         assertEquals(user, lastUser);
+    }
+
+    @Test
+    public void someTest(){
+        User userMock = mock(User.class);
+        when(userMock.getName()).thenReturn("test");
+        assertEquals("test", userMock.getName());
     }
 }
